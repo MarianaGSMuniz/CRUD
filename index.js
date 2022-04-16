866f3b0ce937be0fc917f1f774e186a5389a7c91
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-04-16 15:53:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const { listen } = require('express/lib/application');
+const server = express ();
+server.use(express.json());
+
+const jogo = ['game'];
+
+
+
+
+
+
+
+server.listen(3000);
+
+
+
