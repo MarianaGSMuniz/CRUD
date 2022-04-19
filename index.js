15d4fed74e5ab8b1dda65d997996441428fb14e7
Mariana Muniz <magsmuniz@outlook.com>
2022-04-19 11:59:43 -0300

    atalização

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ const jogo = ['game'];
 server.get('/ game', (req, res) => {
     const { index } = req.params;
     return res.json(game[index]);
+});
 
+// criar novo game
 
+server.post('/ game', req, res => {
+    const (name) = req.body;
+    game.push(name);
+    return res.json(game);
+});
 
-})
+// atualizar game
+
+server.put('/game/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    game[index] = name;
+
+    return res.json(game);
+});
+
+//deletarjogos
+
 
 
 
